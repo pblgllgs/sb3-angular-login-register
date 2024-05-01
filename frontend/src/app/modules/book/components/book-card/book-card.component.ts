9aba82632ad49b0ec5f991031cf721f863fe7159
pblgllgs <pbl.gllgs@gmail.com>
2024-04-30 22:12:45 -0400

    book card component

--- frontend/src/app/modules/book/components/book-card/book-card.component.ts
+++ frontend/src/app/modules/book/components/book-card/book-card.component.ts
@@ -1,31 +1,68 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { BookResponse } from 'src/app/services/models';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
-  styleUrls: ['./book-card.component.scss']
+  styleUrls: ['./book-card.component.scss'],
 })
 export class BookCardComponent {
   private _book: BookResponse = {};
+  private _manage = false;
   private _bookCover: string | undefined;
+
+  @Output() private share: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private archive: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private addToWaitingList: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private borrow: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private edit: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private details: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+
+  public get manage() {
+    return this._manage;
+  }
+
+  @Input()
+  public set manage(value) {
+    this._manage = value;
+  }
 
   public get book(): BookResponse {
     return this._book;
   }
+
   @Input()
   public set book(value: BookResponse) {
     this._book = value;
   }
 
   public get bookCover(): string | undefined {
-    if(this._book.cover){
+    if (this._book.cover) {
       return 'data:image/jpg;base64, ' + this._book.cover;
     }
     return 'https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=';
   }
+
   public set bookCover(value: string | undefined) {
     this._bookCover = value;
   }
 
+  onArchive() {
+    this.archive.emit(this._book);
+  }
+  onShare() {
+    this.share.emit(this._book);
+  }
+  onEdit() {
+    this.edit.emit(this._book);
+  }
+  onAddWaitingList() {
+    this.addToWaitingList.emit(this._book);
+  }
+  onBorrow() {
+    this.borrow.emit(this._book);
+  }
+  onShowDetails() {
+    this.details.emit(this._book);
+  }
 }
